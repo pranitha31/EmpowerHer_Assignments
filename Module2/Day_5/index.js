432eaa599e5ca4a772b154327f06f44a7bf75a21
pranitha <chikkelapranitha@gmail.com>
2025-11-13 13:20:12 +0530

    quation 11

---
+++ Module2/Day_5/index.js
@@ -0,0 +1,15 @@
+let num=2;
+function checkPrime(num){
+    let count=0
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count++
+        }
+    }
+    if(count==2){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(checkPrime(num))
