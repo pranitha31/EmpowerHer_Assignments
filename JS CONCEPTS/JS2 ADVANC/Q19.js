43599b7a7aab5ad9b4b4a11a269fc395bbc97f42
pranitha <chikkelapranitha@gmail.com>
2025-11-25 19:01:26 +0530

    Understanding Global and Function Execution Context

---
+++ JS CONCEPTS/JS2 ADVANC/Q19.js
@@ -0,0 +1,13 @@
+let age=20;
+function displayAge(){
+    console.log("Age is:",age);
+
+}
+    function changeAge(){
+        age=25;
+        console.log("Updated age is:",age);
+    }
+
+displayAge();
+changeAge();
+displayAge();
