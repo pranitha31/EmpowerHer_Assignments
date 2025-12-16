7cd79db074bc1197dddcb4e489a4efdaeb54dfdf
pranitha <chikkelapranitha@gmail.com>
2025-12-16 21:49:12 +0530

    Multi-Component Page

---
+++ React/Q2/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <p style={{ fontSize: "14px", color: "gray", textAlign: "center" }}>
+      This is Footer
+    </p>
+  );
+}
+
+export default Footer;
---
+++ React/Q2/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <h2 style={{ backgroundColor: "#e3e3e3", padding: "10px" }}>
+      This is Navbar
+    </h2>
+  );
+}
+
+export default Navbar;
