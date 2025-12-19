0c634b52169e8bbe5d8ba364ca7c2cff15dd66d5
pranitha <chikkelapranitha@gmail.com>
2025-12-19 22:08:25 +0530

    using useeffectadded

---
+++ React/useEffect/code/src/UserData.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+function UserData() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+    </div>
+  );
+}
+
+export default UserData;
